19be7445fb5360e7c8ff4b6a77c17d4e6b4f42ee
{dschene} <{1071325@hr.nl}>
2023-02-08 11:38:30 +0100

    added mirrored chef

--- scripts/pull_testje.py
+++ scripts/pull_testje.py
@@ -1,2 +1,13 @@
+from PIL import Image
+
 print("Hello Danny")
 print("Nice commit")
+
+with Image.open("chef.png") as img:
+    img.load()
+
+converted_img = img.transpose(Image.FLIP_LEFT_RIGHT)
+
+converted_img.show()
+
+converted_img.save("mirror_chef.png")
