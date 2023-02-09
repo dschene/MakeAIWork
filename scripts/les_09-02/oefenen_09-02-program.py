319bbe9b506d64c73d07c2acdd1d81ec6010f75c
{dschene} <{1071325@hr.nl}>
2023-02-09 10:59:30 +0100

    added input function to 09-02 script

---
+++ scripts/les_09-02/oefenen_09-02-program.py
@@ -0,0 +1,17 @@
+print(type(11/3) == int)
+
+
+print((11%3) == 0)
+
+#define number in variable
+int_1 = eval(input("Enter a number: "))
+#while number is larger than zero -> subtract 3
+while int_1 > 0:
+    int_1 -= 3
+#check if defined variable equals zero,
+if int_1 == 0:
+    #print True when condition is met
+    print(True)
+    #print False when condition is not met
+else:
+    print(False)
